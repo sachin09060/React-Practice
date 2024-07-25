bf11060715f99e02a3ce13b0ce7772a80e8f1c87
sachin09060 <sachinsacchi734@gmail.com>
2024-07-25 11:21:23 +0530

    card animation added

---
+++ TypeScript Examples/card/src/componets/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
